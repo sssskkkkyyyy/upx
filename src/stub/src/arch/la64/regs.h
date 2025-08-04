467a7c374e91e30d7623f88a111d51ee5ee7d501
周海天 <zhou-ht22@mails.tsinghua.edu.cn>
2025-08-04 16:50:02 +0800

    feat:add entry.S

---
+++ src/stub/src/arch/la64/regs.h
@@ -0,0 +1,26 @@
+/* NOTE: THE FIRST ARGUMENT IS arg1, NOT arg0. */
+#define arg1   r4
+#define arg2   r5
+#define arg3   r6
+#define arg4   r7
+#define arg5   r8
+#define arg6   r9
+
+#define t0     r12
+#define t1     r13
+#define t2     r14
+#define t3     r15
+#define t4     r16
+#define t5     r17
+#define t6     r18
+#define t7     r19
+#define t8     r20
+
+#define ret1   r4  /* return value */
+#define ret2   r5  /* return value 2nd word if needed */
+
+#define sp     r3
+#define tp     r2
+#define ra     r1
+#define zero   r0
+#define fp     r22
